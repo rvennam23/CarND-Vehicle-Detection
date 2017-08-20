aa02a96394b35045865aecb950e28e661e98e2de
Ravi Kumar Vennam <rvennam@qti.qualcomm.com>
2017-08-19 22:42:11 -0700

    Add video output
    Add video output

---
+++ detect_vehicle.py
@@ -0,0 +1,151 @@
+import numpy as np
+import cv2
+from moviepy.editor import VideoFileClip
+from sklearn.externals import joblib
+import pickle
+from scipy.ndimage.measurements import label
+from lesson_function import get_hog_features,color_hist,convert_color
+
+heatmaps = []
+heatmap_sum = np.zeros((720, 1280)).astype(np.float64)
+
+def find_cars(img, scale, ystart, ystop, pix_per_cell, cell_per_block, orient, spatial_size, hist_bins,X_scaler,svc):
+
+    draw_img = np.copy(img)
+    # Make a heatmap of zeros
+    heatmap = np.zeros_like(img[:,:,0])
+    img = img.astype(np.float32)/255
+
+    img_tosearch = img[ystart:ystop,:,:]
+    ctrans_tosearch = convert_color(img_tosearch)
+    if scale != 1:
+        imshape = ctrans_tosearch.shape
+        ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1]/scale), np.int(imshape[0]/scale)))
+
+    ch1 = ctrans_tosearch[:,:,0]
+    ch2 = ctrans_tosearch[:,:,1]
+    ch3 = ctrans_tosearch[:,:,2]
+
+    # Define blocks and steps as above
+    nxblocks = (ch1.shape[1] // pix_per_cell) - 1
+    nyblocks = (ch1.shape[0] // pix_per_cell) - 1
+    nfeat_per_block = orient*cell_per_block**2
+    window = 64
+    nblocks_per_window = (window // pix_per_cell) - 1
+    cells_per_step = 1
+    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
+    nysteps = (nyblocks - nblocks_per_window) // cells_per_step
+
+    # Compute individual channel HOG features for the entire image
+    hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+
+    for xb in range(nxsteps):
+        for yb in range(nysteps):
+            ypos = yb*cells_per_step
+            xpos = xb*cells_per_step
+            hog_feat1 = hog1[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel()
+            hog_feat2 = hog2[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel()
+            hog_feat3 = hog3[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel()
+            hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
+
+            xleft = xpos*pix_per_cell
+            ytop = ypos*pix_per_cell
+
+            # Extract the image patch
+            subimg = cv2.resize(ctrans_tosearch[ytop:ytop+window, xleft:xleft+window], (64, 64))
+
+            # Get color features
+            #spatial_features = bin_spatial(subimg, size=spatial_size)
+            hist_features = color_hist(subimg, nbins=hist_bins)
+
+            # Scale features and make a prediction
+            #test_features = X_scaler.transform(np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))
+            test_features = X_scaler.transform(np.hstack(( hist_features, hog_features)).reshape(1, -1))
+            test_prediction = svc.predict(test_features)
+
+            if test_prediction == 1:
+                xbox_left = np.int(xleft*scale)
+                ytop_draw = np.int(ytop*scale)
+                win_draw = np.int(window*scale)
+                cv2.rectangle(draw_img, (xbox_left, ytop_draw+ystart), 
+                    (xbox_left+win_draw, ytop_draw+win_draw+ystart), (0, 0, 255), 6)
+                heatmap[ytop_draw+ystart:ytop_draw+win_draw+ystart, xbox_left:xbox_left+win_draw] += 1
+
+    return draw_img, heatmap
+
+
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    return heatmap
+
+
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0, 0, 255), 6)
+    # Return the image
+    return img
+
+
+def process_image(img):
+    
+    
+    # Define feature parameters
+    orient = 9
+    pix_per_cell = 8
+    cell_per_block = 2
+    spatial_size = (16, 16)
+    hist_bins = 32
+
+
+
+    ystart = 380
+    ystop = 656
+    scale = 1.5
+    out_img, heat_map = find_cars(img, scale=scale, ystart=ystart, ystop=ystop,
+                            pix_per_cell=pix_per_cell, cell_per_block=cell_per_block,
+                            orient=orient, spatial_size=spatial_size, hist_bins=hist_bins,X_scaler=X_scaler,svc=svc)
+
+    global heatmap_sum
+    global heatmaps
+    heatmap_sum = heatmap_sum + heat_map
+    heatmaps.append(heat_map)
+    if len(heatmaps)>15:
+        old_heatmap = heatmaps.pop(0)
+        heatmap_sum -= old_heatmap
+        heatmap_sum = np.clip(heatmap_sum,0.0,1000000.00)
+    heatmap_avg = np.divide(heatmap_sum, len(heatmaps))
+    heatmap_avg_thresh = apply_threshold(heatmap_avg, 2.5)
+    labels = label(heatmap_avg_thresh)
+    # Draw bounding boxes on a copy of the image
+    draw_img = draw_labeled_bboxes(np.copy(img), labels)
+    
+    return draw_img
+
+
+if __name__=='__main__':
+    
+    global svc
+    global X_scaler
+
+    # Load classifier for predicting cars
+    svc = joblib.load('classifier.pkl')
+    X_scaler = pickle.load(open('scaler.p', 'rb'))
+
+    # Detect cars in project video and save output
+    test_output = 'project_video_output.mp4'
+    clip = VideoFileClip('project_video.mp4')
+    test_clip = clip.fl_image(process_image)
+    test_clip.write_videofile(test_output, audio=False)
+
